d661f2ded7e8906ffae18dd208f9e715d397464f
amirrezabsh <amirrezabsh79@gmail.com>
2024-07-08 19:18:48 +0330

    completed dataset loader

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,4 +1,5 @@
 import tensorflow_datasets as tfds
+import tensorflow as tf
 import os
 
 class DatasetLoader:
@@ -13,9 +14,9 @@ class DatasetLoader:
             os.makedirs(self.dataset_path, exist_ok=True)
             
             
-            dataset = self.download_dataset()
+            dataset, info = self.download_dataset()
             
-            return dataset
+            return dataset, info
     
     def download_dataset(self):
         if 'voc' in self.dataset_name:
@@ -23,5 +24,17 @@ class DatasetLoader:
             
             print(f'Load completed: {self.dataset_name}\n{info}')
             
-            return dataset
+            return dataset, info
         
+    def preprocess(self, example):
+        # Define preprocessing steps here
+        image = tf.image.resize(example['image'], (416, 416))
+        image = tf.cast(image, tf.float32) / 255.0
+        bbox = example['objects']['bbox']
+        label = example['objects']['label']
+        return image, bbox, label
+
+    def get_dataset(self, batch_size=32):
+        dataset = self.dataset.map(self.preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.shuffle(1024).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+        return dataset
